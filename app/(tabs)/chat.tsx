4ecc10e3880280d5dc5b4d3ece800996362f5544
r-yvan <yvankiliye.rubuto@gmail.com>
2024-12-04 19:07:39 +0200

    Commit

--- app/(tabs)/chat.tsx
+++ app/(tabs)/chat.tsx
@@ -1,24 +1,28 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, ScrollView } from "react-native";
 import React from "react";
 import { IconArrowNarrowLeft } from "@tabler/icons-react-native";
 
 const translate = () => {
   return (
-    <View className="flex-1 justify-between items-center px-3 py-2">
-      <View className="">
-        <IconArrowNarrowLeft />
-        <Image
-          source={require("~/assets/images/profile-image.jpg")}
-          resizeMode="contain"
-          className="h-full"
-        />
-        <View>
-          <Text>John Abraham</Text>
-          <Text>Active now</Text>
+    <ScrollView className="flex-1 bg-[#0F0F0F]">
+      <View className="flex-1 justify-between items-center px-3 py-2">
+        <View className="">
+          <IconArrowNarrowLeft size={28} color="#FFFFFF50" />
+          <View className="h-20 w-20">
+            <Image
+              source={require("~/assets/images/profile-image.jpg")}
+              resizeMode="contain"
+              className="h-20 w-20"
+            />
+          </View>
+          <View>
+            <Text>John Abraham</Text>
+            <Text>Active now</Text>
+          </View>
         </View>
+        <View></View>
       </View>
-      <View></View>
-    </View>
+    </ScrollView>
   );
 };
 
