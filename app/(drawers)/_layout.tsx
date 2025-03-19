eb9ff952b819fe00ddc4964fdd37c288b3834d33
r-yvan <yvankiliye.rubuto@gmail.com>
2025-03-19 11:57:56 +0200

    Commit

--- app/(drawers)/_layout.tsx
+++ app/(drawers)/_layout.tsx
@@ -1,19 +1,60 @@
 import { View, Text } from "react-native";
+import CustomDrawer from "~/components/CustomDrawer";
 import React from "react";
 import { Drawer } from "expo-router/drawer";
-import { useSegments } from "expo-router";
+import { IconLogout2, IconSettings } from "@tabler/icons-react-native";
 
 const DrawerLayout = () => {
-  const segments = useSegments();
-  const exclude = ["tabs", "(drawers)"];
-
   return (
-    <Drawer  screenOptions={{ headerShown: false }}>
-      {segments
-        .filter((segment) => !exclude.includes(segment))
-        .map((segment) => (
-          <Drawer.Screen key={segment} name={segment} />
-        ))}
+    <Drawer
+      drawerContent={CustomDrawer}
+      screenOptions={{
+        headerShown: false,
+        drawerStyle: {
+          width: 250,
+          backgroundColor: "#151515",
+        },
+        drawerActiveBackgroundColor: "#404040",
+      }}
+    >
+      <Drawer.Screen
+        name="settings"
+        options={{
+          drawerLabel: "Settings",
+          drawerItemStyle: {
+            width: "100%",
+            borderRadius: 15,
+          },
+          drawerLabelStyle: {
+            color: "#FFFFFF",
+            fontSize: 16,
+            fontFamily: "Poppins-Regular",
+          },
+          drawerIcon: () => <IconSettings size={30} color="#FFFFFF" />,
+        }}
+      />
+      <Drawer.Screen
+        name="logout"
+        options={{
+          drawerLabel: "Logout",
+          drawerItemStyle: {
+            width: "100%",
+            borderRadius: 15,
+          },
+          drawerLabelStyle: {
+            color: "#FFFFFF",
+            fontSize: 16,
+            fontFamily: "Poppins-Regular",
+          },
+          drawerIcon: () => <IconLogout2 size={30} color="#FFFFFF" />,
+        }}
+      />
+      <Drawer.Screen
+        name="(tabs)"
+        options={{
+          drawerItemStyle: { display: "none" },
+        }}
+      />
     </Drawer>
   );
 };
--- components/CustomDrawer.tsx
+++ components/CustomDrawer.tsx
@@ -1,10 +1,21 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
+import {
+  DrawerItemList,
+  DrawerItem,
+  DrawerContentScrollView,
+} from "@react-navigation/drawer";
 
-const CustomDrawer = () => {
+const CustomDrawer = ({props}: {props: any}) => {
   return (
     <View>
+      <View>
+        <Image source={require("~/assets/images/profile-image.jpg")} alt="" resizeMode="contain"/>
+      </View>
       <Text>CustomDrawer</Text>
+      <DrawerContentScrollView {...props}>
+        <DrawerItemList {...props} />
+      </DrawerContentScrollView>
     </View>
   );
 };
